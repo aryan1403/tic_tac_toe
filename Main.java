7a469a60a7e0d8f3054138f30239ba66d6de7b03
Hellboy-Aaryan <aaryan14032006@gmail.com>
2021-06-02 21:47:10 +0530

    Updated Runner

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
-public class Main
-{
-	public static void main(String[] args)
-	{
-		new Game();
+import Screens.HomeScreen;
+
+public class Main {
+	public static void main(String[] args) {
+		new HomeScreen();
 	}
 }
